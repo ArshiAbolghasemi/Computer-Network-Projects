93dcd975395cc84b38615ac05652e8cd43e252ac
BehradElmi <behradelmi1@gmail.com>
2023-03-20 23:54:29 +0330

    fixed the makefile

--- socket-programming/src/lib/EntityRepository.cpp
+++ socket-programming/src/lib/EntityRepository.cpp
@@ -1,24 +1,6 @@
 #include "../../include/lib/EntityRepository.hpp"
 
 template<class T>
-template<typename U>
-T* EntityRepository<T>::getByField(std::string key, U fieldValue)
-{
-    SPJsonService* service = new SPJsonService();
-    nlohmann::json jsonData = service->readfile(T::getJsonFilePath());
-    nlohmann::json doc = service->getDocByKeyValue<U>(
-        jsonData[T::getTableName()],
-        key, 
-        fieldValue);
-        
-    if(doc.is_null()){
-        return nullptr;
-    }
-
-    return T::getInstance(doc);
-}
-
-template<class T>
 T* EntityRepository<T>::getById(int id)
 {
     return this->getByField<int>("id", id);
